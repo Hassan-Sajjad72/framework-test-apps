7ee44672340fd5d3d70e72a993eb25e224d4d489
Hassan-Sajjad72 <232460@students.au.edu.pk>
2026-08-27 14:34:19 +0500

    feat: add Astro Node MongoDB fixture

--- JavaScript_Apps/Remix/Remix_Node_PostgreSQL/app/root.jsx
+++
@@ -1,1 +0,0 @@
-import{Links,Meta,Outlet,Scripts,ScrollRestoration}from'@remix-run/react';export default function App(){return <html><head><Meta/><Links/></head><body><Outlet/><ScrollRestoration/><Scripts/></body></html>}
