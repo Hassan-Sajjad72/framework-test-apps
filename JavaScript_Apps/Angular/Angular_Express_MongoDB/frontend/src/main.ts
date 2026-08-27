ab7a7ae7bfca15804d8ee64d0df9ab9def4363fb
Hassan-Sajjad72 <232460@students.au.edu.pk>
2026-08-27 14:34:20 +0500

    feat: add Nuxt SSR PostgreSQL fixture

--- JavaScript_Apps/Angular/Angular_Express_MongoDB/frontend/src/main.ts
+++
@@ -1,4 +0,0 @@
-import { Component } from "@angular/core";
-import { bootstrapApplication } from "@angular/platform-browser";
-@Component({selector:"app-root",standalone:true,template:"<h1>Angular Fixture</h1>"}) class App {}
-bootstrapApplication(App);
--- Python_Apps/Django/Django_ASGI_MySQL/config/settings.py
+++
@@ -1,4 +0,0 @@
-import os
-from dotenv import load_dotenv
-load_dotenv(); SECRET_KEY=os.environ['DJANGO_SECRET_KEY']; DEBUG=False; ALLOWED_HOSTS=['*']; ROOT_URLCONF='config.urls'; MIDDLEWARE=[]; INSTALLED_APPS=[]; TEMPLATES=[]
-DATABASES={'default':{'ENGINE':'django.db.backends.mysql','NAME':os.environ['DB_NAME'],'USER':os.environ['DB_USER'],'PASSWORD':os.environ['DB_PASSWORD'],'HOST':os.environ['DB_HOST'],'PORT':os.environ['DB_PORT']}}
